86c452e9dd2909912cbd60bb1fa006b1af1bbe89
Sarah03 <271220460+SarahO3@users.noreply.github.com>
2026-04-30 07:34:54 +0100

    Update fibonacci exercise
    Use arr for the exercise
    As the loop runs
    It sums the las two index
    And returns it as the last index

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,28 +1,21 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = (arr) => arr.reduce((t, n) => t + n, 0);
+
+const multiply = (arr) => arr.reduce((t, n) => t * n, 1);
+
+const power = (a, b) => a ** b;
+
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
-// Do not edit below this line
 module.exports = {
   add,
   subtract,
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,39 @@
-const fibonacci = function() {
+// const fibonacci = function(n) {
+//   n = Number(n); 
 
-};
+//   if (n < 0) return "OOPS";
+//   if (n === 0) return 0;
+//   if (n === 1) return 1;
 
-// Do not edit below this line
+//   let a = 1;
+//   let b = 1
+
+//   for (let i = 3; i < n; i++) {
+//     let next = a + b;
+//     a = b;
+//     b = next
+//   }
+
+//   return b
+// };
+
+
+
+function fibonacci(n){
+  n = Number(n);
+  if(n < 0) return "OOPS";
+  if(n === 0) return 0;
+   if (n === 1) return 1;
+
+  let arr = [1, 1];
+
+  for(let i = 2; i < n; i++){
+    arr.push(arr[i - 1] + arr[i - 2])
+  }
+  return arr[n - 1]
+}
+
+
+
+
 module.exports = fibonacci;
